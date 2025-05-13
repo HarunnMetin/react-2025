97498aea33abdaa692e02906efa4eed6f443ab78
HarunnMetin <mtnmusa07@gmail.com>
2025-05-14 00:31:27 +0300

    props

--- 3.props/src/App.jsx
+++ 3.props/src/App.jsx
@@ -1,19 +1,27 @@
 import "./App.css";
-import Product from './Product'
+import Container from "./container";
+import Product from "./Product";
 
 function App() {
-
   const productName = "Tisort";
+
   return (
     <div>
-      {/* <Product productName ="Ayakkabi" price = {2000} />
-      <hr />
-      <Product productName= "Pantolon" price= {1200}/>
-      <Product productName={productName} price={350}/> */}
-
-      
+      <Container>
+        <Product productName="Telefon" price = {230000}/>
+      </Container>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     {/* <Product productName ="Ayakkabi" price = {2000} />
+  //     <hr />
+  //     <Product productName= "Pantolon" price= {1200}/>
+  //     <Product productName={productName} price={350}/> */}
+
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ 3.props/src/container.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Container({children}) {
+
+    
+    
+  return (
+    <div>
+      <div>Container Componenti Calisti</div>
+      {children}
+    </div>
+  );
+}
+
+export default Container
